f89442ac79010c8d3bd5de0677b76c09761a9696
I-Mahfouf <143210485+I-Mahfouf@users.noreply.github.com>
2024-03-30 23:47:49 +0100

    Suppression des espaces inutiles

--- front-end/src/redux/actions/authActions.js
+++ front-end/src/redux/actions/authActions.js
@@ -2,25 +2,25 @@
 
 // Action pour gérer la connexion de l'utilisateur //
 export const userLogin = (userData) => {
-    return {
-      type: 'USER_LOGIN',
-      payload: userData,
-    };
+  return {
+    type: 'USER_LOGIN',
+    payload: userData,
   };
+};
   
-  // Action pour vérifier la connexion de l'utilisateur //
-  export const setLoggedIn = (isLoggedIn) => {
-    return {
-      type: 'SET_LOGGED_IN',
-      payload: isLoggedIn,
-    };
+// Action pour vérifier la connexion de l'utilisateur //
+export const setLoggedIn = (isLoggedIn) => {
+  return {
+    type: 'SET_LOGGED_IN',
+    payload: isLoggedIn,
   };
+};
   
-  // Action pour gérer la déconnexion de l'utilisateur //
-  export const userLogout = () => {
-    localStorage.removeItem('token');
-    sessionStorage.removeItem('token');
-    return {
-      type: 'LOGOUT',
-    };
+// Action pour gérer la déconnexion de l'utilisateur //
+export const userLogout = () => {
+  localStorage.removeItem('token');
+  sessionStorage.removeItem('token');
+  return {
+    type: 'LOGOUT',
   };
+};
